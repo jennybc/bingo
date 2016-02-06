086bbe45fb0cfc23f5e659371016c254a1d3595a
jennybc <jenny@stat.ubc.ca>
2016-02-05 23:13:12 -0800

    suggest shiny

--- R/launch.R
+++ R/launch.R
@@ -3,6 +3,10 @@
 #' @param format Which format to generate the cards (downloadable PDF or
 #' printable HTML)
 launch <- function(format = c("pdf", "html")) {
+  if (!requireNamespace("shiny", quietly = TRUE)) {
+    stop("Install shiny via install.packages(\"shiny\") to run this function.",
+         call. = FALSE)
+  }
   format <- match.arg(format)
   folderName <- sprintf("shiny-%s", format)
   shiny::runApp(system.file(folderName, package = "bingo"),
