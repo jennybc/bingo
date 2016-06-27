5e7146a9da42814646b55b5adfb06b48c6eebafd
Gabrielle Weinrott <gabrielle.weinrott@gmail.com>
2016-06-27 11:40:54 -0700

    new july 4th bingo card

---
+++ inst/topics/july4th.R
@@ -0,0 +1,29 @@
+## Aim : bingo card proposal for July 4th
+## Author : Gabrielle Weinrott
+
+c(
+  "Barbecue",
+  "Neighborhood",
+  "America",
+  "Declaration of Independence",
+  "Fireworks",
+  "Parade",
+  "Red, white and blue",
+  "Sunburn",
+  "USA",
+  "Water balloons",
+  "Swimming",
+  "Nation",
+  "July 4th",
+  "Day off",
+  "crackle",
+  "Banners",
+  "Anthem",
+  "Camping",
+  "Stars",
+  "Stripes",
+  "Liberty",
+  "Flag",
+  "Patriot",
+  "Summer"
+)
