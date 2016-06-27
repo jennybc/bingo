0bbaf6253d5a9553df6621ef696aa8a875ea75e4
Satish Joshi <satish23059@gmail.com>
2016-06-27 11:35:36 -0700

    Initial commit of Where Are You From

---
+++ inst/topics/where-are-you-from.R
@@ -0,0 +1,20 @@
+c(
+"Ireland",
+"Scotland",
+"New Zealand",
+"Kenya",
+"Malaysia",
+"Mongolia",
+"Jamaica",
+"Morocco",
+"Nigeria",
+"Venezuela",
+"Ukraine",
+"Argentina",
+"Austria",
+"Switzerland",
+"USA",
+"Greece"
+)
+
+#, Belgium, Turkey, Thailand, India, Egypt, Tanzania, Wales, Portugal, Israel, Ghana
