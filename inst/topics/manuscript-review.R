7970c82542335f5834f463d54b418bfae491cd64
haleyhedlin <haleyhedlin@gmail.com>
2016-06-27 11:45:36 -0700

    Added bingo topics for a statistical manuscript reviewer.

---
+++ inst/topics/manuscript-review.R
@@ -0,0 +1,24 @@
+# Common issues in manuscript reviews
+# Submitted by Haley Hedlin
+c("No limitations described",
+  "Figure legends missing",
+  "Statistical software not specified",
+  "Acronym not defined",
+  "Not clear what test used to calculate a result",
+  "Table/figure not mentioned in results",
+  "Parameterization of statistical model not clear",
+  "CONSORT diagram missing",
+  "Inclusion/exclusion criteria unclear",
+  "Not clear whether analyses chosen a priori",
+  "Handling of missing data not described",
+  "No confidence intervals provided",
+  "Not clear if model assumptions were checked",
+  "Bad graphs (eg pie charts, 3d bar charts)",
+  "Hypotheses not clear",
+  "Level of significance reported",
+  "Unclear statistical methods",
+  "Unclear or missing baseline characteristics/Table 1",
+  "Improper use of statistical terms",
+  "Not clear if significant results are clinically or statistically significant",
+  "Measured values expressed to the appropriate precision"
+)
