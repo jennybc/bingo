7b58758adfec3bbf0f432ec6d0656298ab14dd6e
Kevin Wright <kw.stat@gmail.com>
2016-06-30 06:47:06 -0700

    Create r-conference.R
    Adding topics for useR conference.

---
+++ inst/topics/r-conference.R
@@ -0,0 +1,29 @@
+# Bingo for R conferences, Hard version.
+# Submitted by Kevin Wright
+c(
+"R t-shirt, NOT from useR",
+"useR t-shirts from 3 conferences",
+"Three members of R-core",
+"Slides made with default beamer template",
+"Animated cat/dog clip",
+"Talk not really about R",
+"VisualStudio (not RStudio)",
+"Failure of live demo",
+"Plot made by Excel",
+"Pie chart",
+"Talk with standing room only",
+"tidyverse/Hadleyverse in print",
+"Power vampire (unplugs another laptop)",
+"Non-R code (C, Fortran, Python)",
+"Talk about a package not yet released",
+"Laptop with 5 R stickers",
+"Graphic using rainbow palette",
+"'SAS is better than R' (or Julia, Python)",
+"Talk using two R datasets (iris, Titanic, mtcars, etc)",
+"'Ripleyed'",
+"Book being autographed",
+"devtools",
+"S-Plus",
+"SAS/Stata/SPSS/Minitab results",
+"Someone playing bingo"
+)
