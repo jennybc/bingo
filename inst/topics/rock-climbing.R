4d6839381d6e1ae5473702b73cc8c35b5aeb289f
Anna Fryjoff-Hung <afryjoff-hung@ucmerced.edu>
2018-02-11 18:14:57 -0800

    My own bingo card

---
+++ inst/topics/rock-climbing.R
@@ -0,0 +1,31 @@
+# Things you expect to encounter when rock climbing
+# Authors (GitHub handles):
+# afryjoffhung
+# For ES207, Spring 2018, UC Merced
+
+c (
+"Jug",
+"Sloper",
+"Crimp",
+"Mantle",
+"Gaston",
+"Side-Pull",
+"Lay-Back",
+"Knee Bar",
+"Smear",
+"Edge",
+"Hand Jam",
+"Pinch",
+"Flake",
+"Crack",
+"Bolt",
+"Rope",
+"Quickdraw",
+"Harness",
+"Crash pad",
+"Helmet",
+"Chalk Bag",
+"Shoes",
+"Tape",
+"Undercling"
+)
