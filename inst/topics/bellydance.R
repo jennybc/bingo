c14799db5c7cad653a05d3b2e412777bc59793ae
cade9 <c.ade92@gmail.com>
2018-02-14 11:54:16 -0800

    new bingo card bellydance

---
+++ inst/topics/bellydance.R
@@ -0,0 +1,29 @@
+## Aim : bingo card proposal for belly dance
+## Author : Christiana
+
+c(
+  "Hip shimmy",
+  "Snake arms",
+  "Hip bump",
+  "Vibration",
+  "Veil",
+  "Sword",
+  "Isis Wings",
+  "Zill",
+  "Raqs Sharqi",
+  "Suhaila Salimpour",
+  "Didem",
+  "Sadie",
+  "American Tribal Style",
+  "Tsifteteli",
+  "3/4 shimmy",
+  "Chest bump",
+  "Hip Drop",
+  "Choo Choo Shimmy",
+  "Figure Eight",
+  "Zar Head",
+  "Maya",
+  "Hafla",
+  "Coin Belt",
+  "Bedlah"
+)
