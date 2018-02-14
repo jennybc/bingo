711965d2eb5a0e5092d5c01978d3c8a92bfa70d4
Alan Cai <acai4@ucmerced.edu>
2018-02-14 14:15:37 -0800

    Change to General Tso's chicken

---
+++ ChineseRestaurant.R
@@ -0,0 +1,22 @@
+# Chinese restaurant
+#Author: Alan Cai
+c(
+
+  ## gameplay, generic
+  "Giant fish tank",
+  "Bad English translations",
+  "Arguing over who pays",
+  "Lazy Susan",
+  "take out box with a big smiley face on it",
+  "Asian person helping non-asian friends order",
+  "Kids playing video games",
+  "FREE",
+  "Burning tounge on hot tea",
+  "Extremely old magazines in waiting area",
+  "Family reunion",
+  "Kpop playing on TV",
+  "Hair in soup",
+  "Paying in cash",
+  "Impatient waitress or waiter",
+  "Cheap imitation paintings on wall"
+)
