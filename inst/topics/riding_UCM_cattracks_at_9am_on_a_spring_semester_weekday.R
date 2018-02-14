151f454157c9fe2d70fdeda9c43a21dcb8ef87bb
LILLIE K PENNINGTON <lpennington@ucmerced.edu>
2018-02-13 16:39:53 -0800

    my bingo card!

---
+++ inst/topics/riding_UCM_cattracks_at_9am_on_a_spring_semester_weekday.R
@@ -0,0 +1,29 @@
+## Aim : bingo card proposal for riding UCM cattracks at 9am on a spring semester weekday
+## Author : Lillie Pennington
+
+c(
+  "bus is late",
+  "it is cold",
+  "someone's delicious breakfast is making you hungry",
+  "someone is playing music on their phone for everyone to hear: it is bad",
+  "tule fog",
+  "bus is full: you must stand",
+  "bus driver is a rough rider: spill your drink",
+  "bus breaks down",
+  "birds are singing at bus stop: peaceful",
+  "feel ostracized bc no one wants to sit with you",
+  "finish your book early: have nothing to do for the rest of the ride",
+  "forget book at home",
+  "misplace cat card: can't get on bus",
+  "you make it to school with time to spare: love the cattracks",
+  "stinky breakfast makes you hungry",
+  "seatmate smells good",
+  "can hear someone's music from their headphones across the bus: early onset hearing loss",
+  "the bus driver wishes everyone a good day: wholesome",
+  "bus is very hot: you begin to sweat",
+  "bus is too cold: hypothermia sets in",
+  "running to the bus bc you are late",
+  "see a hawk's nest in a eucalyptus tree: natgeo",
+  "make conversation with the kids at the bus stop",
+  "take wrong bus home and walk 45min home"
+)
