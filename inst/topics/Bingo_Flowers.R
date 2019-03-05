c43c55b6b90f3d5c109236be46ce961a70f62b4f
brittbarreto <43016971+brittbarreto@users.noreply.github.com>
2019-03-05 13:46:01 -0800

    Add files via upload

---
+++ inst/topics/Bingo_Flowers.R
@@ -0,0 +1,71 @@
+---
+title: "Flowers Bingo Card"
+author: "Brittany Lopez Barreto"
+date: "March 4, 2019"
+output: html_document
+---
+
+```{r setup, include=FALSE}
+knitr::opts_chunk$set(echo = TRUE)
+```
+
+
+## Aim : bingo card proposal for flowers
+
+## Author : Brittany Lopez Barreto
+
+
+```{r}
+c(
+
+  "Lily",
+
+  "Rose",
+
+  "Carnation",
+
+  "Baby's Breath",
+
+  "Orchid",
+
+  "Marigold",
+
+  "Sunflower",
+
+  "Hydrangea",
+
+  "Tulip",
+
+  "Peony",
+
+  "Daisy",
+
+  "Alstromeria",
+
+  "Lavendar",
+
+  "Hibiscus",
+
+  "Birds of Paradise",
+
+  "Moonflower",
+
+  "Pansy",
+
+  "Hyacinth",
+
+  "Petunia",
+
+  "Poppy",
+
+  "Snapdragon",
+
+  "Violet",
+
+  "Cosmos",
+
+  "Calendula"
+
+)
+
+```
