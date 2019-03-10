f21000789a79adf4cbc1e0be5fd2e3a1d95f8b64
brittbarreto <43016971+brittbarreto@users.noreply.github.com>
2019-03-10 14:20:12 -0700

    R Flowers Bingo Card (#24)
    * Add files via upload
    
    * Add files via upload
    
    * Remove newlines; 'lavendar' --> 'lavender'

---
+++ inst/topics/Bingo_Flowers.R
@@ -0,0 +1,28 @@
+## Aim : bingo card proposal for flowers
+## Author : Brittany Lopez Barreto
+c(
+  "Lily",
+  "Rose",
+  "Carnation",
+  "Baby's Breath",
+  "Orchid",
+  "Marigold",
+  "Sunflower",
+  "Hydrangea",
+  "Tulip",
+  "Peony",
+  "Daisy",
+  "Alstromeria",
+  "Lavender",
+  "Hibiscus",
+  "Birds of Paradise",
+  "Moonflower",
+  "Pansy",
+  "Hyacinth",
+  "Petunia",
+  "Poppy",
+  "Snapdragon",
+  "Violet",
+  "Cosmos",
+  "Calendula"
+)
