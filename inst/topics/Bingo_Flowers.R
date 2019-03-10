bb53e96e9a33e123b425002456240b4910bf85b6
Jennifer (Jenny) Bryan <jenny.f.bryan@gmail.com>
2019-03-10 14:14:22 -0700

    Remove newlines; 'lavendar' --> 'lavender'

--- inst/topics/Bingo_Flowers.R
+++ inst/topics/Bingo_Flowers.R
@@ -1,58 +1,28 @@
-
 ## Aim : bingo card proposal for flowers
-
 ## Author : Brittany Lopez Barreto
-
-
-
 c(
-
   "Lily",
-
   "Rose",
-
   "Carnation",
-
   "Baby's Breath",
-
   "Orchid",
-
   "Marigold",
-
   "Sunflower",
-
   "Hydrangea",
-
   "Tulip",
-
   "Peony",
-
   "Daisy",
-
   "Alstromeria",
-
-  "Lavendar",
-
+  "Lavender",
   "Hibiscus",
-
   "Birds of Paradise",
-
   "Moonflower",
-
   "Pansy",
-
   "Hyacinth",
-
   "Petunia",
-
   "Poppy",
-
   "Snapdragon",
-
   "Violet",
-
   "Cosmos",
-
   "Calendula"
-
 )
