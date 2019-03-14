c555fd1296bb6d86991da1deb70d7a1f548907ce
Samantha Toet <samanthactoet@gmail.com>
2019-03-13 22:46:19 -0400

    testing the length of a topic

--- tests/testthat/test-package.R
+++ tests/testthat/test-package.R
@@ -4,3 +4,9 @@ test_that("duplicate squares are removed", {
   x <- c("a", "a", "b")
   expect_identical(c("a", "b"), vet_squares(x))
 })
+
+
+test_that("each topic contains 24+ unique values", {
+  x <- letters[1:23]
+  expect_error(bingo(words = x), "m >= n_sq is not TRUE")
+})
