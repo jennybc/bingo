727fc84a86760dcffa5203e43ee15caaf6e5fa3a
Amy Yang <amy.yang@uptake.com>
2019-04-05 17:44:26 -0500

    add test for topic length check: greather than 24

--- tests/testthat/test-package.R
+++ tests/testthat/test-package.R
@@ -4,3 +4,17 @@ test_that("duplicate squares are removed", {
   x <- c("a", "a", "b")
   expect_identical(c("a", "b"), vet_squares(x))
 })
+
+topic_len_gte24 <- function(){
+  topics <- get_topics()
+  topic_len <- c()
+  for (t in topics){
+    topic_len[t] <- length(get_topic(t))
+  }
+  return(ifelse(all(topic_len > 24), TRUE, FALSE))
+}
+
+test_that("each topic contains 24+ unique values", {
+
+  expect_true(topic_len_gte24(), "Submitted topic has <=24 entries")
+})
