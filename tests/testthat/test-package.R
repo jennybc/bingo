b62ed7ac6dc17d7726fed142f8aff8b934aeac8e
Amy Yang <yangy.ustc@gmail.com>
2019-04-06 11:26:39 -0500

    add test for topic length check: greather than 24 (#34)
    * add test for topic length check: greather than 24
    
    * Create a custom expectation

--- tests/testthat/test-package.R
+++ tests/testthat/test-package.R
@@ -4,3 +4,17 @@ test_that("duplicate squares are removed", {
   x <- c("a", "a", "b")
   expect_identical(c("a", "b"), vet_squares(x))
 })
+
+test_that("each topic contains at least 24 unique values", {
+  expect_topic_length <- function(topic) {
+    nt <- length(get_topic(topic))
+    expect(
+      nt >= 24,
+      sprintf("'%s' has length %i", topic, nt)
+    )
+    invisible(topic)
+  }
+
+  topics <- get_topics()
+  for (t in topics) expect_topic_length(t)
+})
