56c8cb245c7a61c982b7a0b5bbb22947273d7592
David W. Body <davidbody@bigcreek.com>
2019-04-14 13:48:40 -0500

    Added an item to the open-data topic (#36)

--- inst/topics/open-data.R
+++ inst/topics/open-data.R
@@ -28,5 +28,6 @@ c(
   "metadata expressed as fieldnames",
   "fieldname EDA",
   "regex-driven workflow",
-  "named region non-sequiturs"
+  "named region non-sequiturs",
+  "screenshot of report"
 )
