7a2a65763a06ce4208f627162d6451c5ee4ebbd7
minnesotka <larissa.stanberry@gmail.com>
2019-04-14 11:16:38 -0500

    create bingo card for backyard life

---
+++ inst/topics/MN_backyardLife.R
@@ -0,0 +1,10 @@
+## Aim : bingo card proposal for animals
+## Author : Larissa Stanberry
+c('cat', 'dog', 'snapping turtle',
+  'painted turtle', 'muskrat', 'mallard duck',
+  'chipmonk', 'squirrel', 'deer',
+  'cardinal', 'redwinged blackbird', 'bluejay',
+  'swan', 'deer', 'monarch butterfly',
+  'snake', 'lizard', 'loon',
+  'owl', 'woodpecker', 'crow',
+  'northern cardinal', 'eagle', 'hawk')
