3595a53b8a0a495a97b3ecfdba02cf619b2f9c17
Amy Yang <yangy.ustc@gmail.com>
2019-04-29 17:37:54 -0500

    Update test-package.R

--- tests/testthat/test-package.R
+++ tests/testthat/test-package.R
@@ -5,7 +5,6 @@ test_that("duplicate squares are removed", {
   expect_identical(c("a", "b"), vet_squares(x))
 })
 
-
 test_that("each topic contains at least 24 unique values", {
   expect_topic_length <- function(topic) {
     nt <- length(get_topic(topic))
