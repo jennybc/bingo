173a1ca20246d5bf54a1d0f852cee4cb907d1da5
Amy Yang <amy.yang@uptake.com>
2019-04-29 10:26:50 -0500

    news nerd card #56

---
+++ inst/topics/news-nerd.R
@@ -0,0 +1,29 @@
+### inspired by https://twitter.com/bizweekgraphics/status/1116385470534045696/photo/1
+### address issue #56
+
+c("has spoken at NICAR for the last 3 years",
+  "RTs any map with hillshades",
+  "worships Amanda Cox",
+  "d3 v4 4ever",
+  "some personal news",
+  "googling flexbox",
+  "srccon community call",
+  "relies on a guy named kyng chaos",
+  "Census update dates in calendar",
+  "very strong opinions about the Data Visualization Society",
+  '\"we are not a service desk\"',
+  "Havre de Grace",
+  "writes?",
+  "has googled \"how to center div\" 300 times",
+  "has had canvas on resum for three years but never used it",
+  "blocked by edward tufte",
+  "Knight Foundation fellowship",
+  "loves observable",
+  "has a tinyletter",
+  "spend three weeks installing qgis",
+  "forgot to test in IE",
+  "tweets about cocktails",
+  "snd bronze",
+  "hates obervable",
+  "newsnerdery"
+  )
