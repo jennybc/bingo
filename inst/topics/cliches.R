b435ad965dafe2887bba6d96c3f1bb48a78e3834
hdshea <hdshea@comcast.net>
2021-02-08 15:00:39 -0500

    Added data for a new bingo card with common - mostly American English - cliches.

---
+++ inst/topics/cliches.R
@@ -0,0 +1,28 @@
+## collected cliches
+## hdshea
+c(
+  "Let’s touch base.",
+  "The apple doesn’t fall far from the tree.",
+  "Don’t put all of your eggs in one basket.",
+  "I’m like a kid in a candy store.",
+  "I lost track of time.",
+  "Time heals all wounds.",
+  "We’re not laughing at you, we’re laughing with you.",
+  "Play your cards right.",
+  "Read between the lines.",
+  "Beauty is only skin deep.",
+  "It’s an uphill battle.",
+  "Better safe than sorry.",
+  "You can’t judge a book by its cover.",
+  "Low-hanging fruit.",
+  "The grass is always greener on the other side.",
+  "Ignorance is bliss.",
+  "All that glitters isn't gold.",
+  "Don’t get your knickers in a twist.",
+  "All for one, and one for all.",
+  "Kiss and make up.",
+  "He has his tail between his legs.",
+  "And they all lived happily ever after.",
+  "Cat got your tongue?",
+  "Someone woke up on the wrong side of the bed."
+)
