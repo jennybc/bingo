d5dcd647de0c26ee045ce513c3069c1e4e65cf44
Kate DeMarsh <katedemarsh@gmail.com>
2022-02-02 20:29:11 -0800

    Bingo Contribution, Bachelor

--- R/getbingocards.R
+++ R/getbingocards.R
@@ -1,4 +1,4 @@
 library(bingo)
-tail(get_topic("boring-meeting"))
-bc <-bingo(n_cards=8, words=get_topic("boring-meeting"))
+tail(get_topic("BachelorBingo"))
+bc <-bingo(n_cards=8, words=get_topic("BachelorBingo"))
 plot(bc)
---
+++ inst/topics/BachelorBingo.R
@@ -0,0 +1,32 @@
+## Bachelor Bingo
+## Kate DeMarsh
+
+
+c(
+  "Journey",
+  "Dramatic Back Story",
+  "Tears",
+  "I'm not here to make friends",
+  "Fight breaks out",
+  "Painful pun entrance",
+  "Dramatically looks into Distance",
+  "Dramatic Season Ever",
+  "Eye Roll",
+  "Previous Contestant Shows up",
+  "Advice Given",
+  "Cocktail Party Canceled",
+  "Can I steal them for a sec?",
+  "Nice person goes home",
+  "Fight during 2 on 1",
+  "Helicopter Ride",
+  "Love",
+  "My future husband/wife",
+  "Shopping Date",
+  "Pool Party",
+  "Falling in love with you",
+  "COVID mentioned",
+  "Hardest Decision",
+  "Rose Given Out",
+  "Talks about Ex",
+  "Gift Given"
+)
