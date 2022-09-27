b206b2c17a212adb799ac3d2d2386fdcbc5a129a
Diego Castro <diego.castro@economia.gov.br>
2022-09-27 16:48:55 -0300

    Add card about soccer player

---
+++ inst/topics/soccer_players.R
@@ -0,0 +1,31 @@
+# Selected players from
+# https://en.wikipedia.org/wiki/List_of_top_international_men%27s_football_goal_scorers_by_country
+
+c(
+  "Cristiano Ronaldo",
+  "Lionel Messi",
+  "Ferenc Puskás",
+  "Pelé",
+  "Robert Lewandowski",
+  "Romelu Lukaku",
+  "Luis Suárez",
+  "Robbie Keane",
+  "Didier Drogba",
+  "Edin Džeko",
+  "Zlatan Ibrahimović",
+  "Miroslav Klose",
+  "David Villa",
+  "Landon Donovan",
+  "Samuel Eto'o",
+  "Wayne Rooney",
+  "Thierry Henry",
+  "Robin van Persie",
+  "Alexis Sánchez",
+  "Andriy Shevchenko",
+  "Gareth Bale",
+  "Paolo Guerrero",
+  "Gheorghe Hagi",
+  "Sadio Mané",
+  "Roque Santa Cruz",
+  "George Weah"
+)
