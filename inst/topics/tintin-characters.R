f12372999f54a57297ee2532aba22243527fa19b
Layal Christine Lettry <layal@cynkra.com>
2022-12-30 15:46:09 +0100

    Add Tintin Characters card

---
+++ inst/topics/tintin-characters.R
@@ -0,0 +1,38 @@
+## Aim : bingo card proposal for "The Adventures of Tintin" characters in 2 languages (French, English)
+## Author : Layal C. Lettry
+## Source: https://www.tintin.com/fr/characters
+
+# French
+c(
+  "Tintin",
+  "Milou",
+  "Capitaine Haddock",
+  "Professeur Tournesol",
+  "Dupond et Dupont",
+  "Nestor",
+  "Bianca Castafiore",
+  "Igor Wagner",
+  "Irma",
+  "Oliveira da Figueira",
+  "François de Hadoque",
+  "Rackham le Rouge",
+  # Gangsters
+  "Rastapopoulos",
+  "Allan Thompson",
+  "Ramon Bada",
+  "Alonzo Perez",
+  "Laszlo Carreidas",
+  "Bobby Smiles",
+  # Professors
+  "Professeur Nestor Halambique",
+  "Professeur Hippolyte Calys",
+  "Professeur Paul Cantonneau",
+  "Professeur Philémon Siclone",
+  "Professeur Marc Charlet",
+  # Kids
+  "Abdallah",
+  "Tchang",
+  "Zorino"
+)
+
+
